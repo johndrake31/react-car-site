75c419f478c035279ba1c154bbfea76c1ab93616
Peter May <peter.may@winzana.com>
2021-11-14 13:37:24 +0100

    feat(feat/main) add base files and types

---
+++ my-car-site-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  const hello = (
+    <div>
+      <h1>Hello World</h1>
+   </div>
+  )
+  
+  return (
+   {hello}
+  );
+}
+
+export default App;
