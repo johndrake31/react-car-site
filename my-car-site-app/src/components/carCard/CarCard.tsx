8f009a9d436488923f069ddedbc09c2f5825aefd
Peter May <peter.may@winzana.com>
2021-11-20 21:44:58 +0100

    refactor(feat/main) add carCard for car display passed props and added logic.

--- my-car-site-app/src/components/carCard/CarCard.tsx
+++ my-car-site-app/src/components/carCard/CarCard.tsx
@@ -1,13 +1,104 @@
 import React, { useEffect, useState } from "react";
 import { ICarPublication } from "../../types/ICarPublication";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+/**
+ *Displays a Card and image and has expandable details.
+ * @param props
+ * @returns
+ */
 export const CarCard = (props: any) => {
   const { carObj } = props;
+
   const [car, setCar] = useState<ICarPublication>();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   useEffect(() => {
     setCar(carObj);
   }, [carObj]);
-
-  return <h1>{car?.title}</h1>;
+  const carImage = `http://vps-dd09afd2.vps.ovh.net:7777/images/cars/${car?.image}`;
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            {car?.brand.toUpperCase().slice(0, 1)}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={car?.title}
+        subheader={car?.year.slice(0, 4)}
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={carImage}
+        alt={car?.title}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {car?.description.slice(0, 100)}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Full Discription</Typography>
+          <Typography paragraph>{car?.description}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 };
